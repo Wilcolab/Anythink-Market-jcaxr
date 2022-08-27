84cf255964884adc4c820be399c498b4400f7171
Mohammed Agboola <moh.agboola@gmail.com>
2022-08-27 12:45:24 +0100

    fix: update schema image to required

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -146,10 +146,6 @@ router.post("/", auth.required, function(req, res, next) {
 
       var item = new Item(req.body.item);
 
-      if (!item.image) {
-        return res.sendStatus(400);
-      }
-
       item.seller = user;
 
       return item.save().then(function() {
